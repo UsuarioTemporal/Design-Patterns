58d03e5fceb51ab629de04cac4430f2aeec7b586
UsuarioTemporal <thomtwd@gmail.com>
2019-01-26 21:21:20 -0500

    Creando la interface PersonaDAO

---
+++ java/PatronesDeDiseño/src/extras/inyeccionDeDependecias/dao/interfaz/PersonaDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package extras.inyeccionDeDependecias.dao.interfaz;
+
+import extras.inyeccionDeDependecias.model.Persona;
+
+/**
+ *
+ * @author User
+ */
+public interface PersonaDAO extends DAO<Integer, Persona>{
+    
+}
