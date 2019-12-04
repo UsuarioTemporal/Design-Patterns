049ea14ca02d10b45f4be1115ad8071406be9049
MaurickThom <thomtwd@gmail.com>
2019-12-04 18:51:11 -0500

    singleton

--- python/creacionales/singleton.py
+++ python/creacionales/singleton.py
@@ -6,8 +6,28 @@ class User(object):
             User.__instance = object.__new__(cls)
         return User.__instance
 
-user1 = User()
-print(user1)
-user2 = User()
-print(user1)
-print(user1 is user2)
+# singleton con decoradores
+def singleton(cls):
+    instances = dict()
+    def wrap(*args,**kwargs):
+        if cls not in instances :
+            instances[cls] = cls(*args,**kwargs)
+        return instances[cls]
+    return wrap
+
+@singleton
+class Admin(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+if __name__ == "__main__":
+    user1 = User()
+    print(user1)
+    user2 = User()
+    print(user1)
+    print(user1 is user2)
+    admin1 = Admin('thom',21)
+    print(admin1.__dict__)
+    admin2 = Admin('thom 2',21)
+    print(admin2.__dict__)
